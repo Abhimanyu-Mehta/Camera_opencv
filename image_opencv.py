a07551075e42bf227a1a8224d7348dba03a825e4
Abhimanyu Mehta <66494125+Abhimanyu-Mehta@users.noreply.github.com>
2020-06-07 14:59:49 +0530

    Create image_opencv.py

---
+++ image_opencv.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('image.png', -1)# your image path or name at te palce of 'image.png'
+
+cv2.imshow("image", img)
+k = cv2.waitKey(0)
+if k == 27:
+   cv2.destroyAllWindows()
+elif k == ord('s'):
+   cv2.imwrite('forest.png', img)
+cv2.destroyAllWindows()
